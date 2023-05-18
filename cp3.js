297dfab7d1ee47ba193bc8daa3003e080a561dcf
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-05-18 16:47:01 -0300

    Criação dos objetos para armazenar os dados form

--- cp3.js
+++ cp3.js
@@ -1,6 +1,19 @@
-//RECUPERANDO ELEMENTOS
-const inputPrimeiroNome = document.querySelector("#idName")
-const inputSegundoNome = document.querySelector("#idSurname")
+// RECUPERANDO ELEMENTOS
+const inputName = document.querySelector("#idName")
+const inputSurname = document.querySelector("#idSurname")
 const inputEmail = document.querySelector("#idEmail")
 const inputSenha = document.querySelector("#idSenha")
 const inputConfirmPass = document.querySelector("#idConfirmPass")
+
+// CAPTURANDO OS EVENTOS
+addEventListener("click",(evt)=>{
+
+    // OBJETO PARA 
+    let usuarioLogado = {
+        nameUsuarioLogado : inputName.value,
+        surnameUsuarioLogado : inputSurname.value,
+        emailUsuarioLogado : inputEmail.value,
+        senhaUsuarioLogado : inputSenha.value,
+        corfirmPassUsuarioLogado : inputConfirmPass.value
+    }
+})
