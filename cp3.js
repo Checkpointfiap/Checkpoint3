077e62a725f547f8753e1ef8ee64d9d2670359d4
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-05-18 16:39:04 -0300

    Recuperando elementos do index

---
+++ cp3.js
@@ -0,0 +1,6 @@
+//RECUPERANDO ELEMENTOS
+const inputPrimeiroNome = document.querySelector("#idName")
+const inputSegundoNome = document.querySelector("#idSurname")
+const inputEmail = document.querySelector("#idEmail")
+const inputSenha = document.querySelector("#idSenha")
+const inputConfirmPass = document.querySelector("#idConfirmPass")
