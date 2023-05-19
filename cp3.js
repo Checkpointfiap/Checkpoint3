3af62bf68010c111fb78cbb827f2694c101602ea
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-05-19 06:08:50 -0300

    validação do campo de nome.

--- cp3.js
+++ cp3.js
@@ -10,3 +10,24 @@ const form = document.querySelector("#form")
 
 
 
+addEventListener("click", (evt)=>{
+    console.log(evt)
+    console.log(evt.target)
+    
+        if(inputName.value.length < 5 && inputName.value.length > 0){
+            console.log("errado");
+            inputName.setAttribute("style","border:2px ridge red");
+            msgStatus.innerHTML ="<span><strong><i>Campo nome preenchido incorretamente</i></strong></span>";
+            msgStatus.setAttribute("style", "color:red");
+            inputName.value ="";
+            inputName.placeholder ="Minimo de 5 caracteres";
+        }else if(inputName.value.length >= 5){
+            console.log("certo");
+            inputName.setAttribute("style","border:2px ridge lime");
+            msgStatus.innerHTML="";
+        }else{
+            inputName.setAttribute("style","");
+        }
+
+
+    })
