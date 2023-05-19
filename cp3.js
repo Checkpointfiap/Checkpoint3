bde9d992353686136b16ad22abadbe9d877955cd
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-05-19 06:08:05 -0300

    adição dos demais elementos recuperados

--- cp3.js
+++ cp3.js
@@ -1,19 +1,12 @@
 // RECUPERANDO ELEMENTOS
-const inputName = document.querySelector("#idName")
-const inputSurname = document.querySelector("#idSurname")
-const inputEmail = document.querySelector("#idEmail")
-const inputSenha = document.querySelector("#idSenha")
-const inputConfirmPass = document.querySelector("#idConfirmPass")
+const inputName = document.querySelector("#idName");
+const inputSurname = document.querySelector("#idSurname");
+const inputEmail = document.querySelector("#idEmail");
+const inputSenha = document.querySelector("#idPass");
+const inputConfirmPass = document.querySelector("#idConfirmPass");
+const msgStatus = document.querySelector("#msg");
+const msgName = document.querySelector("#msgName")
+const form = document.querySelector("#form")
 
-// CAPTURANDO OS EVENTOS
-addEventListener("click",(evt)=>{
 
-    // OBJETO PARA 
-    let usuarioLogado = {
-        nameUsuarioLogado : inputName.value,
-        surnameUsuarioLogado : inputSurname.value,
-        emailUsuarioLogado : inputEmail.value,
-        senhaUsuarioLogado : inputSenha.value,
-        corfirmPassUsuarioLogado : inputConfirmPass.value
-    }
-})
+
